6e5ee92d9c71c2d3e22617c790de5e6201080fa7
Guilherme Fujita <fujitaguilherme@gmail.com>
2020-05-20 13:15:42 -0300

    Dashboard page Layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/19476724?s=400&u=92aa4e484ed429c63b03fb2af389bc5a94aed945&v=4"
+            alt="Guilherme Fujita"
+          />
+          <div>
+            <strong>GuilhermeFujita/Laravel-Casa-Codigo</strong>
+            <p>
+              Sistema desenvolvido em Laravel de acordo com o livro da Casa do
+              Código
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
